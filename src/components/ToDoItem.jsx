e5e7e6925d0657447968b4c38661ff6b305832fa
Jorge Paul <jserranopaul@gmail.com>
2025-05-15 20:43:44 +0200

    Revision May2025

---
+++ src/components/ToDoItem.jsx
@@ -0,0 +1,17 @@
+const ToDoItem = ({ todo, toggleTodo }) => {
+  return (
+    <li className='flex items-center mb-2'>
+      <label>
+        <input
+          type='checkbox'
+          checked={todo.completed}
+          onChange={() => toggleTodo(todo.id)}
+          className='mr-2'
+        />
+        {todo.text}
+      </label>
+    </li>
+  );
+};
+
+export default ToDoItem;
